52b907b9680a91a6fcf0f7294cc6f7c484395db3
Md. Atik hasan <109493827+atikhasan007@users.noreply.github.com>
2026-05-08 10:24:45 +0600

    Add files via upload

---
+++ src/research_and_blog_crew/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from research_and_blog_crew.crew import ResearchAndBlogCrew
+
+def run():
+    """
+    Run the crew.
+    """
+    inputs = {
+        'topic': 'AI Agent is Real Life',
+    }
+
+    try:
+        ResearchAndBlogCrew().crew().kickoff(inputs=inputs)
+    except Exception as e:
+        raise Exception(f"An error occurred while running the crew: {e}")
+
